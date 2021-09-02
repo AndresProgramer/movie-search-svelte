0253db4820d8b389e70e693f928e1c0dfc8248c0
Andres Babilonia Torres <andresdevelopercop@gmail.com>
2021-09-01 21:25:21 -0500

    mejorando la App

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,16 @@
 <script>
-	export let name;
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Busca tu pelicula favorita</h1>
+	<p>Es bueno aprender haciedno a veces andres no te enfrasque solo a una forma de hacer las cosas</p>
+	<div></div>
+	
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
+	
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
